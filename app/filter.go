d0d06cd49328ca5043ce4f133341b58e7428a6a6
Erik Tuerke <etuerke@googlemail.com>
2020-10-24 16:07:36 +0200

    state filter

--- app/filter.go
+++ app/filter.go
@@ -3,10 +3,14 @@ package app
 import (
 	"fmt"
 	"regexp"
+	"strconv"
 	"strings"
 )
 
-func defaultCompare(value1 string, value2 string) bool {
+func defaultCompare(value1 string, value2 string, caseInsensitive bool) bool {
+	if caseInsensitive {
+		return strings.ToLower(value1) == strings.ToLower(value2)
+	}
 	return value1 == value2
 }
 
@@ -37,21 +41,53 @@ func init() {
 func (f *Filter) IsMatchingProcess(process *Process) (bool, error) {
 
 	fieldValue := ""
+	caseInsensitive := false
 
 	if strings.HasPrefix(f.field, "metadata.") {
 		fieldValue = process.Config.Metadata[strings.Split(f.field, ".")[1]]
 	} else if strings.HasPrefix(f.field, "state.") {
+		caseInsensitive = true
+		filterField := strings.Split(f.field, ".")[1]
+		switch filterField {
+		case "pid":
+			if process.Info != nil {
+				fieldValue = strconv.Itoa(int(process.Info.GoPsutilProcess.Pid))
+			} else {
+				fieldValue = "-1"
+			}
+		case "running":
+			fieldValue = strconv.FormatBool(process.IsRunning())
+		case "stopped":
+			fieldValue = strconv.FormatBool(!process.IsRunning())
+		case "dirty":
+			if process.Info != nil {
+				fieldValue = strconv.FormatBool(process.Info.Dirty)
+			} else {
+				fieldValue = "false"
+			}
+		case "dirtyConfig":
+			if process.Info != nil {
+				fieldValue = strconv.FormatBool(process.Info.DirtyCommand)
+			} else {
+				fieldValue = "false"
+			}
+		case "dirtyMd5":
+			if process.Info != nil {
+				fieldValue = strconv.FormatBool(len(process.Info.DirtyMd5Hashes) > 0)
+			} else {
+				fieldValue = "false"
+			}
+		}
 	}
 
 	switch f.operator {
 	case "=":
-		return defaultCompare(fieldValue, f.value), nil
+		return defaultCompare(fieldValue, f.value, caseInsensitive), nil
 	case "==":
-		return defaultCompare(fieldValue, f.value), nil
+		return defaultCompare(fieldValue, f.value, caseInsensitive), nil
 	case "!=":
-		return !defaultCompare(fieldValue, f.value), nil
+		return !defaultCompare(fieldValue, f.value, caseInsensitive), nil
 	}
 
 	return false, nil
-
 }
